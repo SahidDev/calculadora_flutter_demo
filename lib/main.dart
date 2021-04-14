6f4b926b38420d9aa180f2e13aaba53d7b03f03e
SahidDev <serm-2000@hotmail.com>
2021-04-14 13:01:21 -0500

    V1 finalized

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:calculadora/screens/input_screen.dart';
-import 'package:flutter/services.dart';
 
 void main() => runApp(Calculadora());
 
--- lib/screens/input_screen.dart
+++ lib/screens/input_screen.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:calculadora/utilities/components/icon_content.dart';
 import 'package:calculadora/utilities/components/reusable_card.dart';
 import 'package:calculadora/utilities/constants.dart';
-import 'package:calculadora/screens/results_screen.dart';
 import 'package:calculadora/utilities/components/bottom_button.dart';
-import 'package:calculadora/utilities/components/round_icon_button.dart';
-import 'package:calculadora/utilities/functions/calculator_brain.dart';
 
-enum Gender {
-  male,
-  female,
-}
 
 class InputScreen extends StatefulWidget {
   @override
@@ -19,10 +10,6 @@ class InputScreen extends StatefulWidget {
 }
 
 class _InputScreenState extends State<InputScreen> {
-  Gender selectedGender = Gender.male;
-  int height = 180;
-  int weight = 60;
-  int age = 20;
 
   @override
   Widget build(BuildContext context) {
@@ -44,90 +31,31 @@ class _InputScreenState extends State<InputScreen> {
               Expanded(
                 child: ReusableCard(
                   onPress: () {
-                    setState(() {
-                      selectedGender = Gender.male;
-                    });
                   },
-                  colour: selectedGender == Gender.male
-                      ? kActiveCardColour
-                      : kInactiveCardColour,
-                  cardChild: IconContent(
-                    size: screenHeight * 0.1,
-                    icon: FontAwesomeIcons.mars,
-                    label: 'MASCULINO',
-                  ),
+                  colour: kActiveCardColour,
+                  cardChild: Container(),
                 ),
               ),
               Expanded(
                 child: ReusableCard(
-                  //height: screenHeight * 0.01,
-                  //width: screenWidth * 0.08,
+
                   onPress: () {
-                    setState(() {
-                      selectedGender = Gender.female;
-                    });
                   },
-                  colour: selectedGender == Gender.female
-                      ? kActiveCardColour
-                      : kInactiveCardColour,
-                  cardChild: IconContent(
-                    size: screenHeight * 0.1,
-                    icon: FontAwesomeIcons.venus,
-                    label: 'FEMENINO',
-                  ),
+                  colour:kActiveCardColour,
+                  cardChild: Container(),
                 ),
               ),
             ],
           )),
           Expanded(
             child: ReusableCard(
-              //height: screenHeight * 0.02,
-              //width: screenWidth * 0.07,
+              onPress: (){},
+
               colour: kActiveCardColour,
               cardChild: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text(
-                    'ALTURA',
-                    style: kLabelTextStyle,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.baseline,
-                    textBaseline: TextBaseline.alphabetic,
-                    children: <Widget>[
-                      Text(
-                        height.toString(),
-                        style: kNumberTextStyle,
-                      ),
-                      Text(
-                        'cm',
-                        style: kLabelTextStyle,
-                      )
-                    ],
-                  ),
-                  SliderTheme(
-                    data: SliderTheme.of(context).copyWith(
-                      inactiveTrackColor: Color(0xFF8D8E98),
-                      activeTrackColor: Colors.white,
-                      thumbColor: Color(0xFFEB1555),
-                      overlayColor: Color(0x29EB1555),
-                      thumbShape:
-                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
-                      overlayShape:
-                          RoundSliderOverlayShape(overlayRadius: 10.0),
-                    ),
-                    child: Slider(
-                      value: height.toDouble(),
-                      min: 120.0,
-                      max: 220.0,
-                      onChanged: (double newValue) {
-                        setState(() {
-                          height = newValue.round();
-                        });
-                      },
-                    ),
-                  ),
+                  Container(),
                 ],
               ),
             ),
@@ -137,98 +65,24 @@ class _InputScreenState extends State<InputScreen> {
               children: <Widget>[
                 Expanded(
                   child: ReusableCard(
-                    // height: screenHeight * 0.03,
-                    // width: screenWidth * 0.07,
+                    onPress: (){},
                     colour: kActiveCardColour,
                     cardChild: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Text(
-                          'PESO',
-                          style: TextStyle(
-                            fontSize: screenHeight * 0.025,
-                            color: Color(0xFF8D8E98),
-                          ),
-                        ),
-                        Text(
-                          weight.toString(),
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: screenHeight * 0.07,
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            RoundIconButton(
-                              icon: FontAwesomeIcons.minus,
-                              onPressed: () {
-                                setState(() {
-                                  weight--;
-                                });
-                              },
-                            ),
-                            SizedBox(
-                              width: screenWidth * 0.04,
-                            ),
-                            RoundIconButton(
-                              icon: FontAwesomeIcons.plus,
-                              onPressed: () {
-                                setState(() {
-                                  weight++;
-                                });
-                              },
-                            ),
-                          ],
-                        ),
+                        Container(),
                       ],
                     ),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
-                    // height: screenHeight * 0.03,
-                    //width: screenWidth * 0.07,
+                    onPress: (){},
                     colour: kActiveCardColour,
                     cardChild: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Text(
-                          'EDAD',
-                          style: TextStyle(
-                            fontSize: screenHeight * 0.025,
-                            color: Color(0xFF8D8E98),
-                          ),
-                        ),
-                        Text(
-                          age.toString(),
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: screenHeight * 0.07,
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            RoundIconButton(
-                                icon: FontAwesomeIcons.minus,
-                                onPressed: () {
-                                  setState(() {
-                                    age--;
-                                  });
-                                }),
-                            SizedBox(
-                              width: screenWidth * 0.04,
-                            ),
-                            RoundIconButton(
-                                icon: FontAwesomeIcons.plus,
-                                onPressed: () {
-                                  setState(() {
-                                    age++;
-                                  });
-                                }),
-                          ],
-                        )
+                        Container(),
                       ],
                     ),
                   ),
@@ -239,19 +93,6 @@ class _InputScreenState extends State<InputScreen> {
           BottomButton(
             buttonTitle: 'CALCULAR',
             onTap: () {
-              CalculatorBrain calc =
-                  CalculatorBrain(height: height, weight: weight);
-
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ResultsScreen(
-                    bmiResult: calc.calculateBMI(),
-                    resultText: calc.getResult(),
-                    interpretation: calc.getInterpretation(),
-                  ),
-                ),
-              );
             },
           ),
         ],
